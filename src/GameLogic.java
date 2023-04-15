0fdb03f96b401a862c1b75336237b4e47b0a6ef8
yongchunl <yongchun.li@student.unimelb.edu.au>
2023-04-15 15:50:49 +1000

    Completely solve the interdependence between classes, by using the delegate interface

---
+++ src/GameLogic.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+/**
+ * Delegation Interface for decoupling Player and ShadowPac to some extent
+ */
+public interface GameLogic {
+
+    Ghost[] getGhostList();
+    Dot[] getDotList();
+    Wall[] getWallList();
+    void gameFailed();
+    void gameSucceeded();
+
+    void letPlayerCheckArround();
+}
